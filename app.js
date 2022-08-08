e56037f2290c0399c7e14e6bb06c909d74637c2c
serkanisyapan <isyapan15@itu.edu.tr>
2022-08-08 11:17:43 -0700

    Adds buttons to be interactive and keep score of the game

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-let playerScore = 0;
-let computerScore = 0;
+let playerScore = 0
+let computerScore= 0
+let gameText = document.querySelector('h3')
 let playersChoice;
 let computersChoice;
 let buttons = document.querySelectorAll('.btn')
 
 buttons.forEach((button) => {
     button.addEventListener('click', ()=>{
-        playersChoice = (button.innerText).toLowerCase()
+        playersChoice = button.id
         playRound(playersChoice, computersChoice)
     })
 })
@@ -20,15 +21,17 @@ const randomComputersChoice = () => {
 function playRound(playersChoice, computersChoice) {
     computersChoice = randomComputersChoice()
     if (playersChoice === computersChoice) {
-        console.log('Tie round')
+        gameText.innerText = 'Tie round'
     } else if ((computersChoice == "rock" && playersChoice == "scissors") ||
         (computersChoice == "scissors" && playersChoice == "paper") ||
         (computersChoice == "paper" && playersChoice == "rock")) {
-        computerScore += 1
-        console.log(`Computer wins the round.`)
+        computerScore+=1;
+        keepComputerScore();
+        gameText.innerText = 'Computer wins the round.'
     } else {
-        playerScore += 1
-        console.log(`You win the round.`)
+        playerScore+=1;
+        keepPlayerScore();
+        gameText.innerText = 'Player wins the round.'
     }
 }
 
@@ -36,23 +39,16 @@ function scoreBoard() {
     console.log(`Player: ${playerScore} Computer:${computerScore}`)
 }
 
-function gameWinner(playerScore, computerScore) {
-    if (playerScore > computerScore) {
-        console.log('****************************')
-        console.log('Player wins the game')
-        scoreBoard()
-    } else if (computerScore > playerScore) {
-        console.log('****************************')
-        console.log('Computer wins the game')
-        scoreBoard()
-    } else {
-        console.log('****************************')
-        console.log('Tie game')
-        scoreBoard()
-    }
+
+function keepPlayerScore () {
+    let playerScoreDisplay = document.querySelector('#p1-score')
+    playerScoreDisplay.textContent = playerScore
 }
 
-
+function keepComputerScore () {
+    let computerScoreDisplay = document.querySelector('#p2-score')
+    computerScoreDisplay.textContent = computerScore
+}
 
 // function game() {
 //     for (let i = 0; i < 5; i++) {
